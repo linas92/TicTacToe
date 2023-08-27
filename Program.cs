c74e05ca6b4351b011c2a536f95bed002db9f565
Linas Norkus <linasnorkus92@gmail.com>
2023-08-27 17:37:04 +0300

    created the matrix "coordinates" of the tic tac toe "X or O mark"

--- Program.cs
+++ Program.cs
@@ -8,6 +8,12 @@ namespace TicTacToe
 {
     class Program
     {
+        static char[,] playField =
+        {
+            {'1', '2', '3' },
+            {'4', '5', '6' },
+            {'7', '8', '9' }
+        };
         static void Main(string[] args)
         {
             SetField();
@@ -18,13 +24,13 @@ namespace TicTacToe
         public static void SetField()
         {
             Console.WriteLine("     |     |     ");
-            Console.WriteLine($"  {1}  |  {2}  |  {3} ");
+            Console.WriteLine($"  {playField[0,0]}  |  {playField[0, 1]}  |  {playField[0, 2]} ");
             Console.WriteLine("_____|_____|_____");
             Console.WriteLine("     |     |     ");
-            Console.WriteLine($"  {1}  |  {2}  |  {3} ");
+            Console.WriteLine($"  {playField[1, 0]}  |  {playField[1, 1]}  |  {playField[1, 2]} ");
             Console.WriteLine("_____|_____|_____");
             Console.WriteLine("     |     |     ");
-            Console.WriteLine($"  {1}  |  {2}  |  {3} ");
+            Console.WriteLine($"  {playField[2, 0]}  |  {playField[2, 1]}  |  {playField[2, 2]} ");
             Console.WriteLine("     |     |     ");
         }
     }
