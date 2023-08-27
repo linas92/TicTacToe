8f1e210e6a524ebf8f8610a60ad56089d8e831ee
Linas Norkus <linasnorkus92@gmail.com>
2023-08-27 18:06:19 +0300

    Created the switch statement for displaying the sign the player chooses. Implicating the tic tac toe's game logic little by little.

--- Program.cs
+++ Program.cs
@@ -14,9 +14,29 @@ namespace TicTacToe
             {'4', '5', '6' },
             {'7', '8', '9' }
         };
+
         static void Main(string[] args)
         {
+            int player = 2;
+            int input = 0;
+            bool isInputValid = true;
+
             SetField();
+
+            do
+            {
+                if (player == 2)
+                {
+                    player = 1;
+                    EnterXorO(player, input);
+                }
+                else if (player == 1)
+                {
+                    player = 2;
+                    EnterXorO(player, input);
+                }
+                
+            } while (isInputValid);
 
             Console.ReadKey();
         }
@@ -24,7 +44,7 @@ namespace TicTacToe
         public static void SetField()
         {
             Console.WriteLine("     |     |     ");
-            Console.WriteLine($"  {playField[0,0]}  |  {playField[0, 1]}  |  {playField[0, 2]} ");
+            Console.WriteLine($"  {playField[0, 0]}  |  {playField[0, 1]}  |  {playField[0, 2]} ");
             Console.WriteLine("_____|_____|_____");
             Console.WriteLine("     |     |     ");
             Console.WriteLine($"  {playField[1, 0]}  |  {playField[1, 1]}  |  {playField[1, 2]} ");
@@ -32,6 +52,49 @@ namespace TicTacToe
             Console.WriteLine("     |     |     ");
             Console.WriteLine($"  {playField[2, 0]}  |  {playField[2, 1]}  |  {playField[2, 2]} ");
             Console.WriteLine("     |     |     ");
+        }
+        public static void EnterXorO(int player, int input)
+        {
+            char playerSign = ' ';
+
+            if (player == 1)
+            {
+                playerSign = 'X';
+            }
+            else if (player == 2)
+            {
+                playerSign = 'O';
+            }
+            switch (input)
+            {
+                case 1:
+                    playField[0, 0] = playerSign;
+                    break;
+                case 2:
+                    playField[0, 1] = playerSign;
+                    break;
+                case 3:
+                    playField[0, 2] = playerSign;
+                    break;
+                case 4:
+                    playField[1, 0] = playerSign;
+                    break;
+                case 5:
+                    playField[1, 1] = playerSign;
+                    break;
+                case 6:
+                    playField[1, 2] = playerSign;
+                    break;
+                case 7:
+                    playField[2, 0] = playerSign;
+                    break;
+                case 8:
+                    playField[2, 1] = playerSign;
+                    break;
+                case 9:
+                    playField[2, 2] = playerSign;
+                    break;
+            }
         }
     }
 }
